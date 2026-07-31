40699470544ba6744142f975abbcdd657346f691
LizVora <lisa.voraberger@gmx.at>
2026-07-31 18:59:56 +0200

    cleaner code

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x = SCREEN_WIDTH/2, y = SCREEN_HEIGHT/2)
+    
+
 
     while True:
         log_state()
@@ -18,8 +23,10 @@ def main():
 
         
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        updatable.update(dt)
+        for drawing in drawable:
+            drawing.draw(screen)
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
        
